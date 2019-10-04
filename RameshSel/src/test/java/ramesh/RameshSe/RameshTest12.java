3527f27c0a4f4eecb9fbd6d56f8cbcd5726fb045
qedgelab6 <qedgelab6@qedgelab6-PC>
2019-10-04 12:49:27 +0530

    previous class modified & New Class added

---
+++ RameshSel/src/test/java/ramesh/RameshSe/RameshTest12.java
@@ -0,0 +1,11 @@
+package ramesh.RameshSe;
+
+public class RameshTest12
+{
+
+	public static void main(String[] args) {
+		System.out.print("vasudea");
+
+	}
+
+}
--- RameshSel/src/test/java/ramesh/RameshSe/VasuDeva.java
+++ RameshSel/src/test/java/ramesh/RameshSe/VasuDeva.java
@@ -5,6 +5,8 @@ public class VasuDeva {
 	public static void main(String[] args) 
 	{
 		System.out.println("Vasu Git Hub");
+		
+		System.out.println("New Class");
 
 	}
 
