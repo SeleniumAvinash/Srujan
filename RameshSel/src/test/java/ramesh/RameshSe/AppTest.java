1a4aba2a3bd063f419cb0f850f70c309dc967723
qedgelab6 <qedgelab6@qedgelab6-PC>
2019-10-04 12:46:20 +0530

    Complete Project

---
+++ RameshSel/src/test/java/ramesh/RameshSe/AppTest.java
@@ -0,0 +1,13 @@
+package ramesh.RameshSe;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+ {
+	@Test
+	public void sample1()
+	{
+		System.out.println("Vasu");
+	}
+	
+}
---
+++ RameshSel/src/test/java/ramesh/RameshSe/VasuDeva.java
@@ -0,0 +1,11 @@
+package ramesh.RameshSe;
+
+public class VasuDeva {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Vasu Git Hub");
+
+	}
+
+}
